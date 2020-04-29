39efd29197fcf45f8628c62eab67ac52a48af707
Abraham Galicia <torres.abraham@live.com>
2020-04-29 12:38:07 -0500

    Create article modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,17 +25,9 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
   end
 
   # PATCH/PUT /articles/1
